c168bbec4285e081d24bff75213a71250209e75b
comeback <comeback.agency.test@gmail.com>
2025-01-24 18:40:22 +0200

    fix

--- Model/resortModel.js
+++ Model/resortModel.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const resortSchema = new mongoose.Schema({
   name: { type: String },
-  label: { type: String },
 });
 const resort = mongoose.model("resort", resortSchema);
 module.exports = resort;

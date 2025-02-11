9d2e07659cfc932feeb68cfd997d9bbba7228779
comeback <comeback.agency.test@gmail.com>
2025-02-11 21:39:23 +0200

    fix

--- Routes/adminRoutes.js
+++ Routes/adminRoutes.js
@@ -7,5 +7,9 @@ router.post("/signup", admin.signup)
 router.get("/adminUsers", admin.adminUsers)
 router.post("/update_user/:id", admin.updateAdmin)
 router.delete("/delete_user/:id", admin.deleteUsers)
+router.get("/get_banners", admin.getBanners)
+router.get("/get_banner_by_id/:id", admin.getBannerById)
+router.post("/create_banner", admin.create_banner)
+router.post("/update_banner/:id", admin.update_banner)
 
 module.exports = router;
